12f4baafa120e3ce53ced81ba0e0e0a0b40ed33d
gilador <gilador84@gmail.com>
2024-11-24 17:49:16 +0200

    working solution

--- script.js
+++ script.js
@@ -1,23 +1,151 @@
+function importFromJson(event) {
+  const file = event.target.files[0];
+  if (!file) return;
+
+  const reader = new FileReader();
+  const clientsContainer = document.querySelector(".clients-container");
+  clientsContainer.innerHTML = "";
+
+  reader.onload = function (e) {
+    const json = JSON.parse(e.target.result);
+
+    json.clients.forEach((client) => {
+      const overallStatus = {};
+      Object.keys(client.overallStatus).forEach((key) => {
+        if (key === "users") {
+          overallStatus[key] = {
+            total: castToType(
+              client.overallStatus[key].total[0],
+              client.overallStatus[key].total[1]
+            ),
+            active: castToType(
+              client.overallStatus[key].active[0],
+              client.overallStatus[key].active[1]
+            ),
+          };
+        } else {
+          overallStatus[key] = castToType(
+            client.overallStatus[key][0],
+            client.overallStatus[key][1]
+          );
+        }
+      });
+
+      addClient(client.client, overallStatus, client.useCases);
+    });
+
+    showToast("Imported from JSON");
+  };
+
+  reader.readAsText(file);
+
+  // Reset the file input to ensure the `change` event is triggered next time
+  event.target.value = "";
+}
+
+function castToType(value, type) {
+  switch (type) {
+    case "number":
+      return Number(value);
+    case "string":
+      return String(value);
+    case "boolean":
+      return value === "true";
+    default:
+      return value;
+  }
+}
+
+function exportToJson() {
+  const clients = [];
+  document.querySelectorAll(".report-container").forEach((container) => {
+    const client = {};
+    const clientTitle = container.querySelector(".client-title");
+    client.client = clientTitle ? clientTitle.textContent : "N/A";
+    const overallStatusElements = container.querySelectorAll("ul li");
+    const overallStatusKeys = Array.from(overallStatusElements).map(
+      (element) => {
+        const key = element.textContent
+          .split(": ")[0]
+          .toLowerCase()
+          .replace(/ /g, "");
+        return key === "users" ? "users" : key;
+      }
+    );
+    client.overallStatus = {};
+
+    overallStatusElements.forEach((element, index) => {
+      const value = element.textContent.split(": ")[1] || "N/A";
+      if (index === 1) {
+        const usersTotalElement = element.querySelector(".users-total");
+        const usersActiveElement = element.querySelector(".users-active");
+        const total = usersTotalElement
+          ? parseInt(usersTotalElement.textContent) || "N/A"
+          : "N/A";
+        const active = usersActiveElement
+          ? parseInt(usersActiveElement.textContent) || "N/A"
+          : "N/A";
+        client.overallStatus[overallStatusKeys[index]] = {
+          total: [total, "number"],
+          active: [active, "number"],
+        };
+      } else {
+        client.overallStatus[overallStatusKeys[index]] = [value, typeof value];
+      }
+    });
+
+    client.overallStatus.nextMilestone =
+      container.querySelector(".next-milestone")?.textContent || "N/A";
+    client.useCases = [];
+    container.querySelectorAll(".use-case-card").forEach((card) => {
+      const useCase = {};
+      card.querySelectorAll("p").forEach((p) => {
+        const key = p.querySelector("strong").textContent.replace(":", "");
+        const value = p.querySelector(".input-data").textContent;
+        useCase[key] = value;
+      });
+      client.useCases.push(useCase);
+    });
+    clients.push(client);
+  });
+
+  const json = JSON.stringify({ clients }, null, 2);
+  const blob = new Blob([json], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "weekly_report.json";
+  a.click();
+  URL.revokeObjectURL(url);
+  showToast("Exported to JSON");
+}
+
 function toggleEdit(button) {
   const container = button.closest(".report-container");
-  const overallStatusElements = container.querySelectorAll(
-    ".section ul li span, .section ul li"
-  );
-  const useCaseElements = container.querySelectorAll(".use-case-card p strong");
+  const inputDataElements = container.querySelectorAll(".input-data");
 
   const handleInputConversion = (elements) => {
     elements.forEach((element) => {
-      const input = element.querySelector("input");
+      const input = element.querySelector("input, textarea"); // Fix selector
+
       if (input) {
         const value = input.value;
-        element.innerHTML = element.innerHTML.split(": ")[0] + ": " + value;
+        element.textContent = value;
       } else {
-        const value = element.textContent.split(": ")[1];
-        const input = document.createElement("input");
+        const value = element.textContent;
+        let input = document.createElement("input");
         input.type = "text";
-        input.className = "editable-input";
+        if (element.classList.contains("short")) {
+          input.className = "editable-input short";
+        } else if (element.classList.contains("med")) {
+          input.className = "editable-input med";
+        } else {
+          input = document.createElement("textarea");
+          input.className = "editable-input long";
+          input.spellcheck = true;
+        }
         input.value = value;
-        element.innerHTML = element.innerHTML.split(": ")[0] + ": ";
+        element.textContent = "";
         element.appendChild(input);
       }
     });
@@ -25,14 +153,12 @@ function toggleEdit(button) {
 
   document.addEventListener("click", function handleClickOutside(event) {
     if (!container.contains(event.target) && container.querySelector("input")) {
-      handleInputConversion(overallStatusElements);
-      handleInputConversion(useCaseElements);
+      handleInputConversion(inputDataElements);
       document.removeEventListener("click", handleClickOutside);
     }
   });
 
-  handleInputConversion(overallStatusElements);
-  handleInputConversion(useCaseElements);
+  handleInputConversion(inputDataElements);
 }
 
 function createClientTemplate(
@@ -41,22 +167,15 @@ function createClientTemplate(
   useCases = []
 ) {
   const {
-    activeUseCases = 0,
+    activeusecases = 0,
     users = { total: 0, active: 0 },
-    voiceAdoption = "N/A",
+    voiceadoption = "N/A",
     satisfaction = "N/A",
-    nextMilestone = "N/A",
+    nextmilestone = "N/A",
   } = overallStatus;
 
   const useCasesHTML = useCases
-    .map((useCase) =>
-      createUseCaseTemplate(
-        useCase.name,
-        useCase.phase,
-        useCase.users,
-        useCase.risksOrBlockers
-      )
-    )
+    .map((useCase) => createUseCaseTemplate(useCase))
     .join("");
 
   return `
@@ -73,22 +192,25 @@ function createClientTemplate(
 
     <div class="client-title">${clientName}</div>
     <div class="content">
-        <div class="section">
+        <div class="section overall">
             <h2>Overall Status</h2>
             <ul>
-                <li>Active Use Cases: ${activeUseCases}</li>
-                <li>Users: ${users.total} (Active: ${users.active})</li>
-                <li>Voice Adoption: ${voiceAdoption}</li>
-                <li>Satisfaction: ${satisfaction}</li>
-                <li>Next Milestone: <span class="next-milestone">${nextMilestone}</span></li>
+                <li>Active Use Cases: <span class="input-data short active-use-cases">${activeusecases}</span></li>
+                <li>Users: <span class="input-data short users-total">${
+                  users.total
+                }</span> (Active: <span class="input-data short users-active">${
+    users.active
+  }</span>)</li>
+                <li>Voice Adoption: <span class="input-data med voice-adoption">${voiceadoption}</span></li>
+                <li>Satisfaction: <span class="input-data med satisfaction">${satisfaction}</span></li>
+                <li>Next Milestone: <span class="input-data long next-milestone">${nextmilestone}</span></li>
             </ul>
         </div>
-        <div class="section">
+        <div class="section cases">
             <div style="display: flex; align-items: center; justify-content: space-between;">
                 <h2>Use Cases</h2>
                 <button class="add-button client-button" onclick="addUseCase(this)">
-                  <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
-                </button>
+<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-table-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12.5 21h-7.5a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v7.5" /><path d="M3 10h18" /><path d="M10 3v18" /><path d="M16 19h6" /><path d="M19 16v6" /></svg>                </button>
             </div>
             <div class="use-cases-container">
                 ${useCasesHTML || createUseCaseTemplate()}
@@ -102,30 +224,41 @@ function deleteClient(button) {
   container.remove();
 }
 
-function deleteUsecase(button) {
-  const container = button.closest(".report-container");
+function deleteUseCase(button) {
+  const container = button.closest(".use-case-card");
   container.remove();
 }
 
 function createUseCaseTemplate(
-  name = "N/A",
-  phase = "N/A",
-  users = "N/A",
-  risksOrBlockers = "N/A"
+  items = {
+    Name: "N/A",
+    Phase: "N/A",
+    Timing: "N/A",
+    Users: "N/A",
+    RisksOrBlockers: "N/A",
+    Notes: "N/A",
+  }
 ) {
   return `
     <div class="use-case-card" style="position: relative;">
-      <button class="remove-button client-button" onclick="deleteUsecase(this)">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2">
-          <path d="M4 7h16"></path>
-          <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
-          <path d="M10 12l4 4m0 -4l-4 4"></path>
+      <button class="remove-button client-button delete-use-case" onclick="deleteUseCase(this)">
+        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>
+      </button>
+      <button class="add-button client-button add-use-case" onclick="addItemToUseCase(this.closest('.use-case-card'))">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-plus">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+          <path d="M12 5l0 14" />
+          <path d="M5 12l14 0" />
         </svg>
       </button>
-      <p><strong>Use Case Name: ${name}</strong></p>
-      <p><strong>Phase: ${phase}</strong></p>
-      <p><strong>Users: ${users}</strong></p>
-      <p><strong>Risks: ${risksOrBlockers}</strong></p>
+      ${Object.keys(items)
+        .map(
+          (key) =>
+            `<p><strong>${
+              key.charAt(0) + key.slice(1)
+            }:</strong> <span class="input-data long">${items[key]}</span></p>`
+        )
+        .join("")}
     </div>`;
 }
 
@@ -141,7 +274,6 @@ function addClient(
       input = prompt("Enter client name:", clientName);
     } while (input === "");
     if (input === null) return;
-    console.log(input);
     clientName = input;
   }
   const clientsContainer = document.querySelector(".clients-container");
@@ -157,36 +289,11 @@ function addClient(
 }
 
 function addUseCase(button) {
-  const useCasesContainer = button
-    .closest(".section")
-    .querySelector(".use-cases-container");
-  const newUseCase = document.createElement("div");
+  const sectionCases = button.closest(".section.cases");
+  const useCasesContainer = sectionCases.querySelector(".use-cases-container");
+  const newUseCase = document.createElement("template");
   newUseCase.innerHTML = createUseCaseTemplate();
-  useCasesContainer.appendChild(newUseCase);
-}
-
-function importFromJson(event) {
-  const file = event.target.files[0];
-  if (!file) return;
-
-  const reader = new FileReader();
-  const clientsContainer = document.querySelector(".clients-container");
-  clientsContainer.innerHTML = "";
-
-  reader.onload = function (e) {
-    const json = JSON.parse(e.target.result);
-
-    json.clients.forEach((client) => {
-      addClient(client.client, client.overallStatus, client.useCases);
-    });
-
-    showToast("Imported from JSON");
-  };
-
-  reader.readAsText(file);
-
-  // Reset the file input to ensure the `change` event is triggered next time
-  event.target.value = "";
+  useCasesContainer.appendChild(newUseCase.content);
 }
 
 function copyToClipboard() {
@@ -246,89 +353,22 @@ function printReport() {
 }
 
 function reportToImage(callback) {
-  const buttons = document.querySelectorAll("button");
-  buttons.forEach((button) => (button.style.display = "none"));
+  exitEditMode();
 
   const clientsContainer = document.querySelector(".clients-container");
   const originalHeight = clientsContainer.style.height;
   clientsContainer.style.height = "auto";
 
   domtoimage
-    .toBlob(clientsContainer)
+    .toBlob(clientsContainer, { bgcolor: "#f4f4f9" })
     .then((blob) => {
-      callback(blob);
       clientsContainer.style.height = originalHeight;
-      buttons.forEach((button) => (button.style.display = "block"));
+      callback(blob);
     })
     .catch((error) => {
-      console.error("Error capturing the image:", error);
       clientsContainer.style.height = originalHeight;
       buttons.forEach((button) => (button.style.display = "block"));
     });
-}
-
-function exportToJson() {
-  const clients = [];
-  document.querySelectorAll(".report-container").forEach((container) => {
-    const client = {};
-    client.client = container.querySelector(".client-title").textContent;
-    client.overallStatus = {
-      activeUseCases: parseInt(
-        container.querySelector("ul li:nth-child(1)").textContent.split(": ")[1]
-      ),
-      users: {
-        total: parseInt(
-          container
-            .querySelector("ul li:nth-child(2)")
-            .textContent.split(": ")[1]
-            .split(" ")[0]
-        ),
-        active: parseInt(
-          container
-            .querySelector("ul li:nth-child(2)")
-            .textContent.split(": ")[2]
-            ?.replace("(", "")
-            .replace(")", "")
-            .replace("Active: ", "") || 0
-        ),
-      },
-      voiceAdoption: container
-        .querySelector("ul li:nth-child(3)")
-        .textContent.split(": ")[1],
-      satisfaction: container
-        .querySelector("ul li:nth-child(4)")
-        .textContent.split(": ")[1],
-      nextMilestone: container.querySelector(".next-milestone").textContent,
-    };
-    client.useCases = [];
-    container.querySelectorAll(".use-case-card").forEach((card) => {
-      const useCase = {};
-      useCase.name = card
-        .querySelector("p:nth-child(1) strong")
-        .textContent.split(": ")[1];
-      useCase.phase = card
-        .querySelector("p:nth-child(2) strong")
-        .textContent.split(": ")[1];
-      useCase.users = card
-        .querySelector("p:nth-child(3) strong")
-        .textContent.split(": ")[1];
-      useCase.risksOrBlockers = card
-        .querySelector("p:nth-child(4) strong")
-        .textContent.split(": ")[1];
-      client.useCases.push(useCase);
-    });
-    clients.push(client);
-  });
-
-  const json = JSON.stringify({ clients }, null, 2);
-  const blob = new Blob([json], { type: "application/json" });
-  const url = URL.createObjectURL(blob);
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = "weekly_report.json";
-  a.click();
-  URL.revokeObjectURL(url);
-  showToast("Exported to JSON");
 }
 
 function showToast(message) {
@@ -391,4 +431,26 @@ function toggleEditMode() {
   const toggleEditButton = document.querySelector(".toggle-edit");
   clientsContainer.classList.toggle("edit-mode");
   toggleEditButton.classList.toggle("edit-mode");
+}
+function addItemToUseCase(useCaseCard) {
+  const key = prompt("Enter the key for the new item:", "New Key");
+  const newItem = document.createElement("p");
+  newItem.innerHTML = `<strong>${key}:</strong> <span class="input-data long">...</span>`;
+  useCaseCard.appendChild(newItem);
+}
+
+function exitEditMode() {
+  const clientsContainer = document.querySelector(".clients-container");
+  const toggleEditButton = document.querySelector(".toggle-edit");
+  const inputDataElements = clientsContainer.querySelectorAll(".input-data");
+
+  inputDataElements.forEach((element) => {
+    const input = element.querySelector("input, textarea");
+    if (input) {
+      element.textContent = input.value;
+    }
+  });
+
+  clientsContainer.classList.remove("edit-mode");
+  toggleEditButton.classList.remove("edit-mode");
 }
